1f86891cabec3bb402dd866ce26265a4cb2ade53
repl.it user <replituser@example.com>
2020-09-16 06:15:26 +0000

    Updates

---
+++ precache-manifest.d26dd8d5d2056975bf34ab495c4d5d38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dc8bd68c0df13217cfa3a65c3af82e8",
+    "url": "/movieapp/index.html"
+  },
+  {
+    "revision": "860034951114d5a58a1e",
+    "url": "/movieapp/static/css/main.fff4ca80.chunk.css"
+  },
+  {
+    "revision": "98b210fb84cbf3a1c3b3",
+    "url": "/movieapp/static/js/2.00718a3d.chunk.js"
+  },
+  {
+    "revision": "860034951114d5a58a1e",
+    "url": "/movieapp/static/js/main.5ae5ce5a.chunk.js"
+  },
+  {
+    "revision": "4e19d316896026f2633d",
+    "url": "/movieapp/static/js/runtime~main.839b6e54.js"
+  }
+]);
